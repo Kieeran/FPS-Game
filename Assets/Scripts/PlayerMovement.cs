a631a7b008fd9259564bd6bc0c041926bb56d3ed
Kieeran <21521856@gm.uit.edu.vn>
2024-12-07 16:54:06 +0700

    Adjust the basic controls of the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,38 +9,29 @@ public class PlayerMovement : NetworkBehaviour
     public float walkSpeed;
     public float sprintSpeed;
 
-    public PlayerAssetsInputs playerAssetsInputs;
+    [SerializeField] private PlayerAssetsInputs playerAssetsInputs;
+    [SerializeField] private Rigidbody rb;
 
-    //public float groundDrag;
+    public float groundDrag;
 
-    //[Header("Crouching")]
-    //public float crouchSpeed;
-    //public float crouchYScale;
-    //private float startYScale;
+    // [Header("Crouching")]
+    // public float crouchSpeed;
 
-    //[Header("Ground Check")]
-    //public float playerHeight;
+    [Header("Ground Check")]
+    public float playerHeight;
     //public LayerMask Ground;
     private bool isGrounded;
 
-    //[Header("keybinds")]
-    //public KeyCode jumpKey = KeyCode.Space;
-    //public KeyCode sprintKey = KeyCode.LeftShift;
-    //public KeyCode crouchKey = KeyCode.LeftControl;
-
     public float jumpForce;
-    //public float jumpCooldown;
-    //public float airMultiplier;
-    //private bool isReadyToJump;
+    public float jumpCooldown;
+    public float airMultiplier;
+    private bool isReadyToJump;
 
     public Transform orientation;
 
     private Vector2 moveInput;
-    private bool isJumped;
 
     private Vector3 moveDirection;
-
-    public Rigidbody rb;
 
     public MovementState state;
 
@@ -52,29 +43,23 @@ public class PlayerMovement : NetworkBehaviour
         air
     }
 
-    // private void Awake()
-    // {
-    //     //rb = GetComponent<Rigidbody>();
-    // }
 
-    // private void Start()
-    // {
-    //     //isReadyToJump = true;
-    //     //startYScale = transform.localScale.y;
-    // }
+    private void Start()
+    {
+        isReadyToJump = true;
+    }
 
     private void Update()
     {
-        //isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f);
 
-        //MyInput();
-        //SpeedControl();
-        //StateHandler();
+        SpeedControl();
+        StateHandler();
 
-        //if (isGrounded)
-        //    rb.drag = groundDrag;
-        //else
-        //    rb.drag = 0f;
+        if (isGrounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0f;
     }
 
     private void FixedUpdate()
@@ -84,55 +69,50 @@ public class PlayerMovement : NetworkBehaviour
         MovePlayer();
     }
 
-    private void MyInput()
-    {
-        // moveInput = new Vector2(
-        //     PlayerInput.Instance.GetMoveInput().x,
-        //     PlayerInput.Instance.GetMoveInput().y);
+    // private void MyInput()
+    // {
+    //     moveInput = new Vector2(
+    //         PlayerInput.Instance.GetMoveInput().x,
+    //         PlayerInput.Instance.GetMoveInput().y);
 
-        // isJumped = PlayerInput.Instance.GetIsJumped();
+    //     isJumped = PlayerInput.Instance.GetIsJumped();
 
-        // if (isJumped && isGrounded)
-        // {
-        //     Jump();
-        //     isGrounded = false;
-        //     PlayerInput.Instance.GetPlayerAssetsInputs().jump = false;
-        // }
+    //     if (isJumped && isGrounded)
+    //     {
+    //         Jump();
+    //         isGrounded = false;
+    //         PlayerInput.Instance.GetPlayerAssetsInputs().jump = false;
+    //     }
 
-        //if (Input.GetKeyDown(crouchKey))
-        //{
-        //    transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
-        //    rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
-        //}
+    //     if (Input.GetKeyDown(crouchKey))
+    //     {
+    //        transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+    //        rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+    //     }
 
-        //if (Input.GetKeyUp(crouchKey))
-        //{
-        //    transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
-        //    rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
-        //}
+    //     if (Input.GetKeyUp(crouchKey))
+    //     {
+    //        transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+    //        rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+    //     }
 
-        //Debug.Log("Space key: " + Input.GetKey(jumpKey));
-    }
+    //     Debug.Log("Space key: " + Input.GetKey(jumpKey));
+    // }
 
     private void StateHandler()
     {
         if (isGrounded)
         {
-            //if (Input.GetKey(sprintKey))
-            //{
-            //    state = MovementState.sprinting;
-            //    moveSpeed = sprintSpeed;
-            //}
-            //else if (Input.GetKey(crouchKey))
-            //{
-            //    state = MovementState.crouching;
-            //    //moveSpeed = crouchSpeed;
-            //}
-            //else
-            //{
-            //    state = MovementState.walking;
-            //    moveSpeed = walkSpeed;
-            //}
+            if (playerAssetsInputs.sprint)
+            {
+                state = MovementState.sprinting;
+                moveSpeed = sprintSpeed;
+            }
+            else
+            {
+                state = MovementState.walking;
+                moveSpeed = walkSpeed;
+            }
             state = MovementState.walking;
             moveSpeed = walkSpeed;
         }
@@ -144,16 +124,13 @@ public class PlayerMovement : NetworkBehaviour
     private void MovePlayer()
     {
         moveInput = playerAssetsInputs.move;
-        // moveInput.x = Input.GetAxis("Horizontal");
-        // moveInput.y = Input.GetAxis("Vertical");
 
         moveDirection = orientation.forward * moveInput.y + orientation.right * moveInput.x;
 
-        rb.AddForce(moveDirection * moveSpeed * 10f, ForceMode.Force);
-        //if (isGrounded)
-        //    rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-        //else
-        //    rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        if (isGrounded)
+            rb.AddForce(moveDirection * moveSpeed * 10f, ForceMode.Force);
+        else
+            rb.AddForce(moveDirection * moveSpeed * 10f * airMultiplier, ForceMode.Force);
     }
 
     private void SpeedControl()
@@ -174,11 +151,8 @@ public class PlayerMovement : NetworkBehaviour
         //Debug.Log("Process jump");
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (!collision.gameObject.CompareTag("Wall"))
-        {
-            isGrounded = true;
-        }
-    }
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     isGrounded = true;
+    // }
 }
