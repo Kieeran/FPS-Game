f71113a9ca22eb95cef3ef84cc935f57a4196db0
Kieeran <21521856@gm.uit.edu.vn>
2024-12-09 10:30:50 +0700

    Check the feature of shooting a ray at a collide

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using PlayerAssets;
 using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    [SerializeField] private GameObject BulletSpawnPoint;
+    [SerializeField] private GameObject hitScanSphere;
+    [SerializeField] private PlayerAssetsInputs playerAssetsInputs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerAssetsInputs.shoot == true) Shoot();
+    }
+
+    void Shoot()
+    {
+        Ray gunRay = new Ray(BulletSpawnPoint.transform.position, BulletSpawnPoint.transform.forward);
+
+        if (Physics.Raycast(gunRay, out RaycastHit hit))
+        {
+            Debug.Log(hit.collider.name);
+        }
     }
 }
