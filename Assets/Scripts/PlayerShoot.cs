653ffec36411b66fa1da078d11b07d16654e6e69
Kieeran <21521856@gm.uit.edu.vn>
2024-12-09 11:20:34 +0700

    Complete the feature to spawn a red sphere at the ray hit collider position when the Player left-clicks

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -8,6 +8,8 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private GameObject BulletSpawnPoint;
     [SerializeField] private GameObject hitScanSphere;
     [SerializeField] private PlayerAssetsInputs playerAssetsInputs;
+    [SerializeField] private float FireCoolDown;
+    private float CurrentCoolDown;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +20,38 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerAssetsInputs.shoot == true) Shoot();
+        if (playerAssetsInputs.shoot == true)
+        {
+            if (CurrentCoolDown <= 0)
+            {
+                CurrentCoolDown = FireCoolDown;
+                Shoot();
+            }
+        }
+        CurrentCoolDown -= Time.deltaTime;
     }
 
     void Shoot()
     {
-        Ray gunRay = new Ray(BulletSpawnPoint.transform.position, BulletSpawnPoint.transform.forward);
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
 
-        if (Physics.Raycast(gunRay, out RaycastHit hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Debug.Log(hit.collider.name);
+            Debug.Log(hit.point);
+
+            GameObject sphere = Instantiate(hitScanSphere);
+            sphere.transform.position = hit.point;
         }
+
+        // Ray gunRay = new Ray(BulletSpawnPoint.transform.position, BulletSpawnPoint.transform.forward);
+
+        // if (Physics.Raycast(gunRay, out RaycastHit hit))
+        // {
+        //     Debug.Log(hit.point);
+
+        //     GameObject sphere = Instantiate(hitScanSphere);
+        //     sphere.transform.position = hit.point;
+        // }
     }
 }
