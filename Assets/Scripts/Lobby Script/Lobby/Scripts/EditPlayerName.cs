57e8100d66bbdab98bfd69b48bc0c61fc5bdd870
Kieeran <21521856@gm.uit.edu.vn>
2024-12-25 18:17:57 +0700

    Delele EventHandler OnNameChanged

--- Assets/Scripts/Lobby Script/Lobby/Scripts/EditPlayerName.cs
+++ Assets/Scripts/Lobby Script/Lobby/Scripts/EditPlayerName.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class EditPlayerName : MonoBehaviour
 {
     public static EditPlayerName Instance { get; private set; }
-    public event EventHandler OnNameChanged;
     [SerializeField] private TextMeshProUGUI playerNameText;
 
     public string playerName = "PlayerName";
@@ -14,50 +13,22 @@ public class EditPlayerName : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-
-        // GetComponent<Button>().onClick.AddListener(() =>
-        // {
-        //     UI_InputWindow.Show_Static(
-        //         "Player Name",
-        //         playerName,
-        //         "abcdefghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUVXYWZ .,-",
-        //         20,
-        //         onCancel,
-        //         onOk
-        //     );
-        // });
     }
 
     private void Start()
     {
-        OnNameChanged += EditPlayerName_OnNameChanged;
-
         playerNameText.text = playerName;
     }
-
-    // private void onCancel()
-    // {
-    //     // Cancel
-    // }
-
-    // private void onOk(string newName)
-    // {
-    //     playerName = newName;
-
-    //     playerNameText.text = playerName;
-
-    //     OnNameChanged?.Invoke(this, EventArgs.Empty);
-    // }
 
     public void ReadPlayerNameInputField(string s)
     {
         playerName = s;
         playerNameText.text = playerName;
 
-        OnNameChanged?.Invoke(this, EventArgs.Empty);
+        UpdatePlayerName();
     }
 
-    private void EditPlayerName_OnNameChanged(object sender, EventArgs e)
+    private void UpdatePlayerName()
     {
         LobbyManager.Instance.UpdatePlayerName(playerName);
     }
