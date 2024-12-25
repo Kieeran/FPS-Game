211fde320449c0d5fdc6cab7c1c03922bb9aeec1
Kieeran <21521856@gm.uit.edu.vn>
2024-12-25 17:45:56 +0700

    Modify some components in the UI_InputWindow scrip
    Remove the 'title' text, comment out most of the functions in the UI_InputWindow script, and add event listeners for the Ok and Cancel buttons.

--- Assets/_/InputWindow/Scripts/UI_InputWindow.cs
+++ Assets/_/InputWindow/Scripts/UI_InputWindow.cs
@@ -9,133 +9,147 @@ public class UI_InputWindow : MonoBehaviour
     private Button okBtn;
     private Button cancelBtn;
 
-    private Action ok_Click = null;
-    private Action cancel_Click = null;
+    // private Action ok_Click = null;
+    // private Action cancel_Click = null;
 
-    private TextMeshProUGUI titleText;
+    // private TextMeshProUGUI titleText;
     private TMP_InputField inputField;
 
     private void Awake()
     {
         instance = this;
 
+        // okBtn = transform.Find("okBtn").GetComponent<Button>();
+        // cancelBtn = transform.Find("cancelBtn").GetComponent<Button>();
+        // titleText = transform.Find("titleText").GetComponent<TextMeshProUGUI>();
+        // inputField = transform.Find("inputField").GetComponent<TMP_InputField>();
+
+        //RegisterEvents();
+
+        //Hide();
+
         okBtn = transform.Find("okBtn").GetComponent<Button>();
         cancelBtn = transform.Find("cancelBtn").GetComponent<Button>();
-        titleText = transform.Find("titleText").GetComponent<TextMeshProUGUI>();
         inputField = transform.Find("inputField").GetComponent<TMP_InputField>();
 
-        RegisterEvents();
-
-        Hide();
-    }
-
-    private void RegisterEvents()
-    {
-        okBtn?.onClick.AddListener(() =>
+        okBtn.onClick.AddListener(() =>
         {
-            ok_Click?.Invoke();
+            Hide();
         });
 
-        cancelBtn?.onClick.AddListener(() =>
+        cancelBtn.onClick.AddListener(() =>
         {
-            cancel_Click?.Invoke();
+            Hide();
         });
     }
+
+    // private void RegisterEvents()
+    // {
+    //     okBtn?.onClick.AddListener(() =>
+    //     {
+    //         ok_Click?.Invoke();
+    //     });
+
+    //     cancelBtn?.onClick.AddListener(() =>
+    //     {
+    //         cancel_Click?.Invoke();
+    //     });
+    // }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            //okBtn.ClickFunc();
-            ok_Click?.Invoke();
-        }
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            //cancelBtn.ClickFunc();
-            cancel_Click?.Invoke();
-        }
+        // if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        // {
+        //     //okBtn.ClickFunc();
+        //     ok_Click?.Invoke();
+        // }
+        // if (Input.GetKeyDown(KeyCode.Escape))
+        // {
+        //     //cancelBtn.ClickFunc();
+        //     cancel_Click?.Invoke();
+        // }
     }
 
-    private void Show(string titleString, string inputString, string validCharacters, int characterLimit, Action onCancel, Action<string> onOk)
-    {
-        gameObject.SetActive(true);
-        transform.SetAsLastSibling();
+    // private void Show(string titleString, string inputString, string validCharacters, int characterLimit, Action onCancel, Action<string> onOk)
+    // {
+    //     gameObject.SetActive(true);
+    //     transform.SetAsLastSibling();
 
-        titleText.text = titleString;
+    //     titleText.text = titleString;
 
-        inputField.characterLimit = characterLimit;
-        inputField.onValidateInput = (string text, int charIndex, char addedChar) =>
-        {
-            return ValidateChar(validCharacters, addedChar);
-        };
+    //     inputField.characterLimit = characterLimit;
+    //     inputField.onValidateInput = (string text, int charIndex, char addedChar) =>
+    //     {
+    //         return ValidateChar(validCharacters, addedChar);
+    //     };
 
-        inputField.text = inputString;
-        inputField.Select();
+    //     inputField.text = inputString;
+    //     inputField.Select();
 
-        // okBtn.ClickFunc = () =>
-        // {
-        //     Hide();
-        //     onOk(inputField.text);
-        // };
+    //     // okBtn.ClickFunc = () =>
+    //     // {
+    //     //     Hide();
+    //     //     onOk(inputField.text);
+    //     // };
 
-        // cancelBtn.ClickFunc = () =>
-        // {
-        //     Hide();
-        //     onCancel();
-        // };
+    //     // cancelBtn.ClickFunc = () =>
+    //     // {
+    //     //     Hide();
+    //     //     onCancel();
+    //     // };
 
-        ok_Click = () =>
-        {
-            Hide();
-            onOk(inputField.text);
-        };
+    //     ok_Click = () =>
+    //     {
+    //         Hide();
+    //         onOk(inputField.text);
+    //     };
 
-        cancel_Click = () =>
-        {
-            Hide();
-            onCancel();
-        };
-    }
+    //     cancel_Click = () =>
+    //     {
+    //         Hide();
+    //         onCancel();
+    //     };
+    // }
 
     private void Hide()
     {
         gameObject.SetActive(false);
     }
 
-    private char ValidateChar(string validCharacters, char addedChar)
-    {
-        if (validCharacters.IndexOf(addedChar) != -1)
-        {
-            // Valid
-            return addedChar;
-        }
-        else
-        {
-            // Invalid
-            return '\0';
-        }
-    }
+    // private char ValidateChar(string validCharacters, char addedChar)
+    // {
+    //     if (validCharacters.IndexOf(addedChar) != -1)
+    //     {
+    //         // Valid
+    //         return addedChar;
+    //     }
+    //     else
+    //     {
+    //         // Invalid
+    //         return '\0';
+    //     }
+    // }
 
-    public static void Show_Static(string titleString, string inputString, string validCharacters, int characterLimit, Action onCancel, Action<string> onOk)
-    {
-        instance.Show(titleString, inputString, validCharacters, characterLimit, onCancel, onOk);
-    }
+    // public static void Show_Static(string titleString, string inputString, string validCharacters, int characterLimit, Action onCancel, Action<string> onOk)
+    // {
+    //     instance.Show(titleString, inputString, validCharacters, characterLimit, onCancel, onOk);
+    // }
 
-    public static void Show_Static(string titleString, int defaultInt, Action onCancel, Action<int> onOk)
-    {
-        instance.Show(titleString, defaultInt.ToString(), "0123456789-", 20, onCancel,
-            (string inputText) =>
-            {
-                // Try to Parse input string
-                if (int.TryParse(inputText, out int _i))
-                {
-                    onOk(_i);
-                }
-                else
-                {
-                    onOk(defaultInt);
-                }
-            }
-        );
-    }
+    // public static void Show_Static(string titleString, int defaultInt, Action onCancel, Action<int> onOk)
+    // {
+    //     instance.Show(titleString, defaultInt.ToString(), "0123456789-", 20, onCancel,
+    //         (string inputText) =>
+    //         {
+    //             // Try to Parse input string
+    //             if (int.TryParse(inputText, out int _i))
+    //             {
+    //                 onOk(_i);
+    //             }
+    //             else
+    //             {
+    //                 onOk(defaultInt);
+    //             }
+    //         }
+    //     );
+    // }
 }
