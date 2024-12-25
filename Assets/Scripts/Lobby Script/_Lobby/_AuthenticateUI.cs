60b22d11abd8fbae4c285d118ba31ecb9a7c52d8
Kieeran <21521856@gm.uit.edu.vn>
2024-12-25 16:48:42 +0700

    Duplicate all scripts in: Assets\Scripts\Lobby Script\Lobby\Scripts then move to: Assets\Scripts\Lobby Script\_Lobby

---
+++ Assets/Scripts/Lobby Script/_Lobby/_AuthenticateUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class _AuthenticateUI : MonoBehaviour
+{
+    [SerializeField] private Button authenticateButton;
+
+    // public static uint editNameFlag = 0;
+
+    private void Awake()
+    {
+        authenticateButton.onClick.AddListener(() =>
+        {
+            LobbyManager.Instance.Authenticate(EditPlayerName.Instance.GetPlayerName());
+            // editNameFlag = 1;
+            // SceneManager.LoadScene("Lobby");
+            //GameSceneManager.Instance.SetPlayerName(LobbyManager.Instance.GetPlayerName());
+            GameSceneManager.Instance.LoadNextScene();
+
+            //Debug.Log("Authenticate button clicked");
+        });
+    }
+
+    // private void Hide()
+    // {
+    //     gameObject.SetActive(false);
+    // }
+}
---
+++ Assets/Scripts/Lobby Script/_Lobby/_LobbyCreateUI.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class _LobbyCreateUI : MonoBehaviour
+{
+    public static _LobbyCreateUI Instance { get; private set; }
+
+    [SerializeField] private Button createButton;
+    [SerializeField] private Button lobbyNameButton;
+    [SerializeField] private Button publicPrivateButton;
+    [SerializeField] private Button maxPlayersButton;
+    [SerializeField] private Button gameModeButton;
+    [SerializeField] private Button returnButton;
+    [SerializeField] private TextMeshProUGUI lobbyNameText;
+    [SerializeField] private TextMeshProUGUI publicPrivateText;
+    [SerializeField] private TextMeshProUGUI maxPlayersText;
+    [SerializeField] private TextMeshProUGUI gameModeText;
+
+
+    private string lobbyName;
+    private bool isPrivate;
+    private int maxPlayers;
+    private LobbyManager.GameMode gameMode;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        returnButton.onClick.AddListener(() =>
+        {
+            Hide();
+            LobbyListUI.Instance.Show();
+        });
+
+        createButton.onClick.AddListener(() =>
+        {
+            LobbyManager.Instance.CreateLobby(
+                lobbyName,
+                maxPlayers,
+                isPrivate,
+                gameMode
+            );
+            Hide();
+        });
+
+        lobbyNameButton.onClick.AddListener(() =>
+        {
+            UI_InputWindow.Show_Static("Lobby Name", lobbyName, "abcdefghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUVXYWZ .,-", 20,
+            () =>
+            {
+                // Cancel
+            },
+            (string lobbyName) =>
+            {
+                this.lobbyName = lobbyName;
+                UpdateText();
+            });
+        });
+
+        publicPrivateButton.onClick.AddListener(() =>
+        {
+            isPrivate = !isPrivate;
+            UpdateText();
+        });
+
+        maxPlayersButton.onClick.AddListener(() =>
+        {
+            UI_InputWindow.Show_Static("Max Players", maxPlayers,
+            () =>
+            {
+                // Cancel
+            },
+            (int maxPlayers) =>
+            {
+                this.maxPlayers = maxPlayers;
+                UpdateText();
+            });
+        });
+
+        gameModeButton.onClick.AddListener(() =>
+        {
+            switch (gameMode)
+            {
+                default:
+                case LobbyManager.GameMode.PvE:
+                    gameMode = LobbyManager.GameMode.PvP;
+                    break;
+                case LobbyManager.GameMode.PvP:
+                    gameMode = LobbyManager.GameMode.PvE;
+                    break;
+            }
+            UpdateText();
+        });
+
+        Hide();
+    }
+
+    private void UpdateText()
+    {
+        lobbyNameText.text = lobbyName;
+        publicPrivateText.text = isPrivate ? "Private" : "Public";
+        maxPlayersText.text = maxPlayers.ToString();
+        gameModeText.text = gameMode.ToString();
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+
+        lobbyName = "MyLobby";
+        isPrivate = false;
+        maxPlayers = 2;
+        gameMode = LobbyManager.GameMode.PvE;
+
+        UpdateText();
+    }
+
+}
