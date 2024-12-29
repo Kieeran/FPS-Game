bd8b6097f64932d03c56910ffe4e91f51c65b446
Kieeran <21521856@gm.uit.edu.vn>
2024-12-29 21:58:46 +0700

    Implement the toggle feature for EscapeUI in the code

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using PlayerAssets;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private PlayerAssetsInputs playerAssetsInputs;
+    [SerializeField] private Image escapeUI;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerAssetsInputs.escapeUI == true)
+        {
+            escapeUI.gameObject.SetActive(!escapeUI.gameObject.activeSelf);
+
+            playerAssetsInputs.escapeUI = false;
+        }
     }
 }
