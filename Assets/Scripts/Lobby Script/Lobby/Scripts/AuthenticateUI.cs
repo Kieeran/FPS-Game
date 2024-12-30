7e1992f1a12a72a5eea3f6af0d3917b7bc446eee
Kieeran <21521856@gm.uit.edu.vn>
2024-12-30 21:50:16 +0700

    Set a condition in the AuthenticateUI script to avoid calling the UnityServices.InitializeAsync method multiple times.

--- Assets/Scripts/Lobby Script/Lobby/Scripts/AuthenticateUI.cs
+++ Assets/Scripts/Lobby Script/Lobby/Scripts/AuthenticateUI.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Services.Core;
 using UnityEngine.SceneManagement;
 
 public class AuthenticateUI : MonoBehaviour
 {
-
     private void Awake()
     {
         GetComponent<Button>().onClick.AddListener(() =>
         {
-            LobbyManager.Instance.Authenticate(EditPlayerName.Instance.GetPlayerName());
             GameSceneManager.Instance.LoadNextScene();
+
+            if (UnityServices.State == ServicesInitializationState.Initialized)
+                return;
+
+            LobbyManager.Instance.Authenticate(EditPlayerName.Instance.GetPlayerName());
         });
     }
 }
