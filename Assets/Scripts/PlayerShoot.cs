d3c665754eedc9debaf7de592fa55adf42bd4777
Quốc Bảo <21521856@gm.uit.edu.vn>
2025-03-18 11:55:04 +0700

    Handle collision detection on the server instead of directly on the client

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -11,6 +11,13 @@ public class PlayerShoot : NetworkBehaviour
     [SerializeField] private GameObject hitEffect;
     [SerializeField] private PlayerAssetsInputs playerAssetsInputs;
     [SerializeField] private float FireCoolDown;
+
+    [SerializeField] private PlayerHead playerHead;
+    [SerializeField] private PlayerBody playerBody;
+
+    public PlayerHead GetPlayerHead() { return playerHead; }
+    public PlayerBody GetPlayerBody() { return playerBody; }
+
     private float CurrentCoolDown;
 
     // Start is called before the first frame update
@@ -35,10 +42,73 @@ public class PlayerShoot : NetworkBehaviour
 
     public void Shoot()
     {
-        if (IsOwner == false) return;
-
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+
+        Shoot_ServerRPC(ray.origin, ray.direction);
+
+        // if (Physics.Raycast(ray, out RaycastHit hit))
+        // {
+        //     // Debug.Log(hit.point);
+
+        //     // GameObject effect = Instantiate(hitEffect);
+        //     // effect.transform.position = hit.point;
+
+        //     // StartCoroutine(DestroyEffect(effect));
+
+        //     BulletHitSpawnServerRpc(hit.point);
+
+        //     PlayerBody playerBody = hit.collider.GetComponent<PlayerBody>();
+        //     PlayerHead playerHead = hit.collider.GetComponent<PlayerHead>();
+
+        //     if (playerBody != null)
+        //     {
+        //         // if (IsOwner == true)
+        //         // {
+        //         //     playerBody.Hit();
+        //         // }
+
+        //         Transform player = hit.collider.transform.parent;
+
+        //         if (player != null)
+        //         {
+        //             NetworkObject networkObject = player.GetComponent<NetworkObject>();
+
+        //             if (networkObject != null)
+        //             {
+        //                 //Debug.Log($"Detech {networkObject.OwnerClientId} body");
+        //                 player.GetComponent<PlayerTakeDamage>().TakeDamage(0.05f, networkObject.OwnerClientId);
+        //             }
+        //         }
+        //     }
+
+        //     if (playerHead != null)
+        //     {
+        //         // if (IsOwner == true)
+        //         // {
+        //         //     playerHead.Hit();
+        //         // }
+
+        //         Transform player = hit.collider.transform.parent.parent;
+
+        //         if (player != null)
+        //         {
+        //             NetworkObject networkObject = player.GetComponent<NetworkObject>();
+
+        //             if (networkObject != null)
+        //             {
+        //                 //Debug.Log($"Detech {networkObject.OwnerClientId} head");
+        //                 player.GetComponent<PlayerTakeDamage>().TakeDamage(0.1f, networkObject.OwnerClientId);
+        //             }
+        //         }
+        //     }
+        // }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void Shoot_ServerRPC(Vector3 point, Vector3 shootDirection)
+    {
+        Ray ray = new(point, shootDirection);
 
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
@@ -49,18 +119,15 @@ public class PlayerShoot : NetworkBehaviour
 
             // StartCoroutine(DestroyEffect(effect));
 
-            BulletHitSpawnServerRpc(hit.point);
+            // BulletHitSpawnServerRpc(hit.point);
+
+            BulletHitSpawn_ClientRpc(hit.point);
 
             PlayerBody playerBody = hit.collider.GetComponent<PlayerBody>();
             PlayerHead playerHead = hit.collider.GetComponent<PlayerHead>();
 
             if (playerBody != null)
             {
-                // if (IsOwner == true)
-                // {
-                //     playerBody.Hit();
-                // }
-
                 Transform player = hit.collider.transform.parent;
 
                 if (player != null)
@@ -69,7 +136,6 @@ public class PlayerShoot : NetworkBehaviour
 
                     if (networkObject != null)
                     {
-                        //Debug.Log($"Detech {networkObject.OwnerClientId} body");
                         player.GetComponent<PlayerTakeDamage>().TakeDamage(0.05f, networkObject.OwnerClientId);
                     }
                 }
@@ -77,11 +143,6 @@ public class PlayerShoot : NetworkBehaviour
 
             if (playerHead != null)
             {
-                // if (IsOwner == true)
-                // {
-                //     playerHead.Hit();
-                // }
-
                 Transform player = hit.collider.transform.parent.parent;
 
                 if (player != null)
@@ -90,7 +151,6 @@ public class PlayerShoot : NetworkBehaviour
 
                     if (networkObject != null)
                     {
-                        //Debug.Log($"Detech {networkObject.OwnerClientId} head");
                         player.GetComponent<PlayerTakeDamage>().TakeDamage(0.1f, networkObject.OwnerClientId);
                     }
                 }
@@ -98,14 +158,14 @@ public class PlayerShoot : NetworkBehaviour
         }
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    public void BulletHitSpawnServerRpc(Vector3 spawnPos)
-    {
-        BulletHitSpawnClientRpc(spawnPos);
-    }
+    // [ServerRpc(RequireOwnership = false)]
+    // public void BulletHitSpawnServerRpc(Vector3 spawnPos)
+    // {
+    //     BulletHitSpawnClientRpc(spawnPos);
+    // }
 
     [ClientRpc]
-    public void BulletHitSpawnClientRpc(Vector3 spawnPos)
+    public void BulletHitSpawn_ClientRpc(Vector3 spawnPos)
     {
         GameObject effect = Instantiate(hitEffect);
         effect.transform.position = spawnPos;
