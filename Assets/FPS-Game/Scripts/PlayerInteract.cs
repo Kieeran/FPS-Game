d6c645ac8c9c027bf2ba46d37cbc8bc649a452ba
Quốc Bảo <21521856@gm.uit.edu.vn>
2025-04-04 19:07:36 +0700

    Add an outline to objects that the player can interact with when they are nearby and looking at them.

---
+++ Assets/FPS-Game/Scripts/PlayerInteract.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using PlayerAssets;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    PlayerAssetsInputs _playerAssetsInputs;
+    Interactable _currentInteractableObj;
+    float _playerReach;
+
+    void Start()
+    {
+        _playerAssetsInputs = GetComponent<PlayerAssetsInputs>();
+
+        _playerReach = 3f;
+    }
+
+    void Update()
+    {
+        CheckInteraction();
+
+        if (_playerAssetsInputs.interact == true)
+        {
+            _playerAssetsInputs.interact = false;
+
+            if (_currentInteractableObj != null)
+                Debug.Log("Interact with " + _currentInteractableObj.name);
+        }
+    }
+
+    void CheckInteraction()
+    {
+        Ray ray = new(Camera.main.transform.position, Camera.main.transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, _playerReach))
+        {
+            if (hit.collider.CompareTag("Interactable"))
+            {
+                Interactable newInteractableObj = hit.collider.GetComponent<Interactable>();
+
+                if (_currentInteractableObj != null)
+                {
+                    if (newInteractableObj == _currentInteractableObj) return;
+
+                    _currentInteractableObj.DisableOutline();
+                }
+
+                SetNewCurrentInteractable(newInteractableObj);
+            }
+
+            else
+            {
+                DisableCurrentInteractable();
+            }
+        }
+
+        else
+        {
+            DisableCurrentInteractable();
+        }
+    }
+
+    void SetNewCurrentInteractable(Interactable interactable)
+    {
+        _currentInteractableObj = interactable;
+        _currentInteractableObj.EnableOutline();
+    }
+
+    void DisableCurrentInteractable()
+    {
+        if (_currentInteractableObj != null)
+        {
+            _currentInteractableObj.DisableOutline();
+            _currentInteractableObj = null;
+        }
+    }
+}
