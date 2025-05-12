9c2133385d236cc49bf8d0319f0adc474add6799
Haiseus <21521836@gm.uit.edu.vn>
2025-05-12 18:55:26 +0700

    Change player prefab, adjust PlayerShoot.cs
    Change player prefab with model name into PlayerModel, adjust a bit of code in PlayerShoot.cs so that it can work for PlayerModel

--- Assets/FPS-Game/Scripts/Player/PlayerShoot.cs
+++ Assets/FPS-Game/Scripts/Player/PlayerShoot.cs
@@ -103,36 +103,64 @@ public class PlayerShoot : NetworkBehaviour
 
             BulletHitSpawn_ClientRpc(hit.point);
 
-            PlayerBody playerBody = hit.collider.GetComponentInChildren<PlayerBody>();
-            PlayerHead playerHead = hit.collider.GetComponentInChildren<PlayerHead>();
+            // PlayerBody playerBody = hit.collider.GetComponentInChildren<PlayerBody>();
+            // PlayerHead playerHead = hit.collider.GetComponentInChildren<PlayerHead>();
 
-            if (playerBody != null)
+            // if (playerBody != null)
+            // {
+            //     Transform player = hit.collider.transform.parent;
+
+            //     if (player != null)
+            //     {
+            //         NetworkObject networkObject = player.GetComponent<NetworkObject>();
+
+            //         if (networkObject != null)
+            //         {
+            //             player.GetComponent<PlayerTakeDamage>().TakeDamage(0.05f, networkObject.OwnerClientId, OwnerClientId);
+            //         }
+            //     }
+            // }
+
+            // if (playerHead != null)
+            // {
+            //     Transform player = hit.collider.transform.parent.parent;
+
+            //     if (player != null)
+            //     {
+            //         NetworkObject networkObject = player.GetComponent<NetworkObject>();
+
+            //         if (networkObject != null)
+            //         {
+            //             player.GetComponent<PlayerTakeDamage>().TakeDamage(0.1f, networkObject.OwnerClientId, OwnerClientId);
+            //         }
+            //     }
+            // }
+
+            Transform rootPlayerTransform = hit.collider.transform;
+
+            while (rootPlayerTransform != null && rootPlayerTransform.GetComponent<NetworkObject>() == null)
             {
-                Transform player = hit.collider.transform.parent;
-
-                if (player != null)
-                {
-                    NetworkObject networkObject = player.GetComponent<NetworkObject>();
-
-                    if (networkObject != null)
-                    {
-                        player.GetComponent<PlayerTakeDamage>().TakeDamage(0.05f, networkObject.OwnerClientId, OwnerClientId);
-                    }
-                }
+                rootPlayerTransform = rootPlayerTransform.parent;
             }
 
-            if (playerHead != null)
+            if (rootPlayerTransform != null)
             {
-                Transform player = hit.collider.transform.parent.parent;
+                PlayerBody playerBody = rootPlayerTransform.GetComponentInChildren<PlayerBody>(true);
+                PlayerHead playerHead = rootPlayerTransform.GetComponentInChildren<PlayerHead>(true);
 
-                if (player != null)
+                if (playerBody == null) Debug.Log("playerBody = null");
+                if (playerHead == null) Debug.Log("playerHead = null");
+
+                if (playerBody != null)
                 {
-                    NetworkObject networkObject = player.GetComponent<NetworkObject>();
+                    NetworkObject netObj = rootPlayerTransform.GetComponent<NetworkObject>();
+                    rootPlayerTransform.GetComponent<PlayerTakeDamage>().TakeDamage(0.05f, netObj.OwnerClientId, OwnerClientId);
+                }
 
-                    if (networkObject != null)
-                    {
-                        player.GetComponent<PlayerTakeDamage>().TakeDamage(0.1f, networkObject.OwnerClientId, OwnerClientId);
-                    }
+                if (playerHead != null)
+                {
+                    NetworkObject netObj = rootPlayerTransform.GetComponent<NetworkObject>();
+                    rootPlayerTransform.GetComponent<PlayerTakeDamage>().TakeDamage(0.1f, netObj.OwnerClientId, OwnerClientId);
                 }
             }
         }
