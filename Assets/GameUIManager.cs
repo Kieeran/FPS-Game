1c3424ac4dbf142b3a2ad13dda7e7ec7ff0f8be2
Haiseus <21521836@gm.uit.edu.vn>
2025-11-07 00:29:16 +0700

    Adjust Lobby List and Behavior Trees
    Adding How To Play UI and Gameplay UI, ESC button for open/close Settings UI in LobbyList. Adjusting Behavior Trees (Combat and Patrol) for better visual.

--- Assets/GameUIManager.cs
+++ Assets/GameUIManager.cs
@@ -5,53 +5,40 @@ using TMPro;
 
 public class GameUIManager : MonoBehaviour
 {
-    [SerializeField] public GameObject exitGamePopUp;
-    [SerializeField] public Button graphicsButton;
-    [SerializeField] public GameObject graphicsSettings;
-    [SerializeField] public Button audioButton;
-    [SerializeField] public GameObject audioSettings;
+    [SerializeField] public GameObject settingsPopUp;
+    // [SerializeField] public Button graphicsButton;
+    // [SerializeField] public GameObject graphicsSettings;
+    // [SerializeField] public Button audioButton;
+    // [SerializeField] public GameObject audioSettings;
+
+    private bool isSettingsOn = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        exitGamePopUp.SetActive(false);
-        dropdown.value = QualitySettings.GetQualityLevel();
+        settingsPopUp.SetActive(false);
+        // dropdown.value = QualitySettings.GetQualityLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Escape))
-        // {
-        //     exitGamePopUp.SetActive(true);
-        //     Cursor.visible = true;
-        //     Cursor.lockState = CursorLockMode.None;
+        // if (gameObject.activeInHierarchy) isSettingsOn = true;
+        // else isSettingsOn = false;
 
-        //     graphicsButton.onClick.AddListener(() =>
-        //     {
-        //         graphicsSettings.SetActive(true);
-        //         audioSettings.SetActive(false);
-        //     });
-
-        //     audioButton.onClick.AddListener(() =>
-        //     {
-        //         graphicsSettings.SetActive(false);
-        //         audioSettings.SetActive(true);
-        //     });
-        // }
-
-        // if (Input.GetKeyDown(KeyCode.F1))
-        // {
-        //     exitGamePopUp.SetActive(false);
-        // }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            settingsPopUp.SetActive(!isSettingsOn);
+            isSettingsOn = !isSettingsOn;
+        }
     }
 
-    public RenderPipelineAsset[] qualityLevels;
-    public TMP_Dropdown dropdown;
+    // public RenderPipelineAsset[] qualityLevels;
+    // public TMP_Dropdown dropdown;
 
-    public void ChangeLevel(int value)
-    {
-        QualitySettings.SetQualityLevel(value, true);
-        QualitySettings.renderPipeline = qualityLevels[value];
-    }
+    // public void ChangeLevel(int value)
+    // {
+    //     QualitySettings.SetQualityLevel(value, true);
+    //     QualitySettings.renderPipeline = qualityLevels[value];
+    // }
 }
